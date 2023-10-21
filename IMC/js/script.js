a0c706ab0024843355cf8aee30b71d4465dd1123
Rafael <rafaelferrete333@gmail.com>
2023-10-21 13:37:48 -0300

    Correção de bugs
    Nessa nova versão todos os bugs foram corrigidos

--- IMC/js/script.js
+++ IMC/js/script.js
@@ -1,55 +1,75 @@
 function calcular() {
     res = document.getElementById('resultado');
     clas = document.getElementById('classificacao');
-    altura = document.getElementById('altura');
-    peso = document.getElementById('peso');
+    altura = document.getElementById('altura').value;
+    peso = document.getElementById('peso').value;
 
-    imc = Number(peso.value) / (Math.pow(Number(altura.value), 2));
+    const tipoPeso = Boolean(Number(peso) < 0);
+    const tipoAltura = Boolean(Number(altura) < 0);
 
-    if (imc == Infinity | imc == 0) 
-    {
+    let imc = peso / (Math.pow(altura, 2));
+    imc = imc.toFixed(2);
+    
+
+    if (tipoPeso == Boolean(true) || tipoAltura == Boolean(true)) {
         res.innerHTML = "<p>Verifique as informações digitadas</p>";
+        clas.innerHTML = "";
     }
     else {
-        if (imc < 18.5) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Abaixo do Peso</p>";
-            clas.style.color = 'tomato';
-        }
-        else if ((imc >= 18.5) & (imc <= 24.9)) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Peso Normal</p>";
-            clas.style.color = 'green';
-        }
-        else if ((imc >= 25) & (imc <= 29.9)) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Sobrepeso</p>";
-            clas.style.color = 'yellow';
-        }
-        else if ((imc >= 30) & (imc <= 34.9)) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Obesidade Grau I</p>";
-            clas.style.color = 'orange';
-        }
-        else if ((imc >= 35) & (imc <= 39.9)) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Obesidade Grau II</p>";
-            clas.style.color = 'tomato';
-        }
-        else if (imc >= 40) 
-        {
-            res.innerHTML = "<p>O seu IMC é " + imc.toFixed(2) + "</p>";
-            clas.innerHTML = "<p>Obesidade Grau III ou Mórbida</p>";
-            clas.style.color = 'red';
-        }
-        else
-        {
+        if (imc == Infinity || imc == 0) {
             res.innerHTML = "<p>Verifique as informações digitadas</p>";
+            clas.innerHTML = "";
+        }
+        else {
+            if (imc < 18.5) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Abaixo do Peso</p>";
+                clas.style.color = 'tomato';
+            }
+            else if ((imc >= 18.5) & (imc <= 24.9)) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Peso Normal</p>";
+                clas.style.color = 'green';
+            }
+            else if ((imc >= 25) & (imc <= 29.9)) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Sobrepeso</p>";
+                clas.style.color = 'yellow';
+            }
+            else if ((imc >= 30) & (imc <= 34.9)) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Obesidade Grau I</p>";
+                clas.style.color = 'orange';
+            }
+            else if ((imc >= 35) & (imc <= 39.9)) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Obesidade Grau II</p>";
+                clas.style.color = 'tomato';
+            }
+            else if (imc >= 40) {
+                imc = imc.toString();
+                imc = imc.replace(".", ",");
+                res.innerHTML = "<p>O seu IMC é " + imc + "</p>";
+                clas.innerHTML = "<p>Obesidade Grau III ou Mórbida</p>";
+                clas.style.color = 'red';
+            }
+            
+            else {
+                res.innerHTML = "";
+                clas.innerHTML = "";
+            }
         }
     }
 }
+
+
